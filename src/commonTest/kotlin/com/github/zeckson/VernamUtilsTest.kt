a94d384e236d917db18e5a90827be629c48dae19
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2022-04-29 07:32:22 +0530

    refactor/move

--- src/commonTest/kotlin/com/github/zeckson/VernamUtilsTest.kt
+++ src/commonTest/kotlin/com/github/zeckson/VernamUtilsTest.kt
@@ -40,40 +40,40 @@ class VernamUtilsTest {
     fun checkRegressionWithDefaultHash() {
         assertEncode("C1n25Cu52\$", encrypt("abcdefghij", hash("abcdefghij")))
         assertEncode(
-                "86Kl1W1dql4vN29vm056",
-                encrypt("lzFT95rQGbfba5GAedoX", hash("lzFT95rQGbfba5GAedoX"))
+            "86Kl1W1dql4vN29vm056",
+            encrypt("lzFT95rQGbfba5GAedoX", hash("lzFT95rQGbfba5GAedoX"))
         )
         assertEncode(
-                "h00rzbb15Zwr42WmN92t",
-                encrypt("OYb2Xlnc1FG5TgWZWPWi", hash("EVDM62G85RrOlJppDTpF"))
+            "h00rzbb15Zwr42WmN92t",
+            encrypt("OYb2Xlnc1FG5TgWZWPWi", hash("EVDM62G85RrOlJppDTpF"))
         )
         assertEncode(
-                "tt@lWfjW7h662K1Wnuzn",
-                encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB"))
+            "tt@lWfjW7h662K1Wnuzn",
+            encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB"))
         )
     }
 
     @Test
     fun checkRegressionWithSHA256() {
         assertEncode(
-                "tt@lWfjW7h662K1Wnuzn",
-                encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "SHA-256"))
+            "tt@lWfjW7h662K1Wnuzn",
+            encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "SHA-256"))
         )
     }
 
     @Test
     fun checkRegressionWithSHA1() {
         assertEncode(
-                "tt@lWfjW7h662K1Wnuzn",
-                encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "SHA-256"))
+            "tt@lWfjW7h662K1Wnuzn",
+            encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "SHA-256"))
         )
     }
 
     @Test
     fun checkRegressionWithMD5() {
         assertEncode(
-                "N2Z34xnWlWlfCmwrnttN",
-                encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "MD5"))
+            "N2Z34xnWlWlfCmwrnttN",
+            encrypt("mlHINlaW5uv7xCiszlCV", hash("vFniW7aivKX8sydozDmB", "MD5"))
         )
     }
 
