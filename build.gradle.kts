2fb8ad74e1ad99376cb52139e60bf0b41344bcaa
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2022-05-18 01:01:00 +0300

    configure npm package

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.zvernam"
-version = "0.3.0-SNAPSHOT"
+version = "0.3.1"
 
 repositories {
     mavenCentral()
@@ -25,6 +25,7 @@ kotlin {
         compilations["main"].packageJson {
             name = "@zvernam/${rootProject.name}"
             version = project.version.toString()
+            customField("repository" to "git://github.com:ZVernam/zvernam-core.git")
         }
 
         nodejs {
